8f53421efa91d47998d7492bd2a6e66b620ba5fc
Isabel <isabel.gogov1@libero.it>
2020-10-28 21:08:27 +0100

    completato

--- js/script.js
+++ js/script.js
@@ -7,43 +7,64 @@
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 var mine = [];
-var controllo = 1;
-
-// CREAZIONE ARRAY MINE (numeri randomici)
-  var random = Math.floor(Math.random() * 100) + 1;
-  mine.push(random);
+var controllo = true;
+var vince, perde;
+var numeriUte = [];
 
   // faccio un ciclo for da 1 a 16
-  for (var i = 0; i < 15; i++) {
+  for (var i = 0; i < 16; i++) {
     // il ciclo while si ripete finche' non viene generato un numero random non presente nell'array
     while (controllo) {
       // genero dei numeri tra 1 e 100
       random = Math.floor(Math.random() * 100) + 1;
-      var boolean = mine.includes(random);
-      if (boolean === false) {
-        controllo = 0;
-      }
+      // richiamo la mia funzione per cercare i numeri doppi
+      controllo = ricerca(random, mine);
     }
 
     // salvo random nell'array
     mine.push(random);
-    controllo = 1;
+    controllo = true;
   }
   console.log(mine);
 
 
-
-// INPUT
   // faccio un ciclo che mi generi un prompt (max 100 - 16 volte)
+  vince = 1;
+  perde = true;
+  controllo = true;
+  while ((vince <= 84) && perde) {
+    // l'utente inserisce un numero e controllo che non sia gia' stato inserito
+    while (controllo) {
+      var input = parseInt(prompt("inserisci un numero da 1 a 100"));
+      controllo = ricerca(input, numeriUte);
+      // controllo se il numero inserito dall'utente e' presente nell'array mine
+      perde = !ricerca(input, mine);
+    }
 
-// CREAZIONE ARRAY UTENTE
-  // faccio un array che tiene traccia dei numeri inseriti dall'utente
+    if (perde) {
+      // se perde, vince non viene incrementato
+      vince++;
 
-  // controllo che il numero inserito non sia presente nell'array
-  // se presente stampo errore e richiedo nuovamente il numero
+      // faccio un array che tiene traccia dei numeri inseriti dall'utente
+      numeriUte.push(input);
+      controllo = true;
+    }
+  }
+  console.log(numeriUte);
 
-  // controllo se il numero inserito dall'utente e' presente nell'array mine
-  // se presente la partita termina e stampo hai perso
-  // altrimenti continua e salva il punteggio
+// OUTPUT
+  // se il numero in input e' presente in mine la partita termina e stampo "hai perso"
+  if (perde === false) {
+    console.log("hai perso");
+  // altrimenti stampo "vittoria"
+  } else {
+    console.log("hai vintoooo!");
+  }
+  // salvo il punteggio ottenuto dall'utente
+  console.log("il tuo punteggio e' ", numeriUte.length);
 
-  // se l'utente non inserisce un numero vietato 84 volte stampo "vittoria"
+
+  // FUNZIONI
+  function ricerca(numero, array){
+    return array.includes(numero);
+  }
