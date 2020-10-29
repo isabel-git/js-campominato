96f923b3838f69431d5358d0272e8d98508bfe14
Isabel <isabel.gogov1@libero.it>
2020-10-29 15:30:11 +0100

    inserito controllo sulle difficolta'

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -5,27 +5,38 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-
-var mine = [];
+var difficolta;
 var controllo = true;
+var mine = [];
 var vince, perde;
 var numeriUte = [];
 var n;
 
 // chiedo all'utente di inserire la difficolta'
-var text;
-var difficolta = prompt("scegli la difficolta' tra 0, 1 o 2");
-
-switch (difficolta) {
-  case "1":
+// con il ciclo while faccio un controllo del numero inserito
+while (controllo) {
+  difficolta = prompt("scegli la difficolta' tra 0, 1 e 2");
+  switch (difficolta) {
+    case "2":
+    controllo = false;
+    n = 50;
+    console.log("l'utente ha scelto la difficolta' ",2);
+    break;
+    case "1":
+    controllo = false;
+    console.log("l'utente ha scelto la difficolta' ",1);
     n = 80;
     break;
-  case "2":
-    n = 50;
-   break;
- default:
+    case "0":
+    controllo = false;
     n = 100;
+    console.log("l'utente ha scelto la difficolta' ",0);
+    break;
+    default:
+    console.log("Numero non valido");
   }
+}
+  controllo = true;
 
   // faccio un ciclo for da 1 a 16
   for (var i = 0; i < 16; i++) {
@@ -37,7 +48,7 @@ switch (difficolta) {
       controllo = ricerca(random, mine);
     }
 
-    // salvo random nell'array
+    // salvo i random nell'array
     mine.push(random);
     controllo = true;
   }
@@ -76,11 +87,11 @@ switch (difficolta) {
   } else {
     console.log("hai vintoooo!");
   }
-  // salvo il punteggio ottenuto dall'utente
-  console.log("il tuo punteggio e' ", numeriUte.length, "/" ,n);
+  // stampo il punteggio ottenuto dall'utente
+  console.log("il tuo punteggio e' ", numeriUte.length, "/" ,n," punti");
 
 
-  // FUNZIONI
-  function ricerca(numero, array){
-    return array.includes(numero);
-  }
+// FUNZIONI
+function ricerca(numero, array){
+  return array.includes(numero);
+}
